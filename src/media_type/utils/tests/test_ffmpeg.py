a0395f0079100a6638fa7d06b8dfccd9aa545d7a
lucemia <lucemia@gmail.com>
2023-07-11 01:05:03 +0800

    add a command that check current ffmpeg version

--- src/media_type/utils/tests/test_ffmpeg.py
+++ src/media_type/utils/tests/test_ffmpeg.py
@@ -1,6 +1,6 @@
 from syrupy.assertion import SnapshotAssertion
 
-from ..ffmpeg import _get_muxer_info, list_support_format
+from ..ffmpeg import _get_muxer_info, get_ffmpeg_version, list_support_format
 
 
 def test_list_support_format(snapshot: SnapshotAssertion) -> None:
@@ -9,3 +9,7 @@ def test_list_support_format(snapshot: SnapshotAssertion) -> None:
 
 def test__get_muxer_info(snapshot: SnapshotAssertion) -> None:
     assert snapshot == _get_muxer_info("6.0", "E", "mp4", "MP4 (MPEG-4 Part 14)")
+
+
+def test_get_ffmpeg_version() -> None:
+    get_ffmpeg_version()
