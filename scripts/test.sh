60e5266f0e8092f04350be20d016cb3e5b94805d
lucemia <lucemia@gmail.com>
2023-07-17 15:12:55 +0800

    fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,8 @@
 
 VERSION=${1:-6.0}
 
-poetry shell
 poetry install --with test
 
 alias ffmpeg='docker jrottenberg/ffmpeg:{version}-scratch'
 
-pytest ./src --cov=./src -s
+poetry run pytest ./src --cov=./src -s
