17c575f4debfb289a46fa66a3eaf47e6d69f1f7d
lucemia <lucemia@gmail.com>
2023-07-21 15:41:20 +0800

    fix test

--- src/ffmpeg_media_type/tests/test_info.py
+++ src/ffmpeg_media_type/tests/test_info.py
@@ -14,10 +14,14 @@ from ..info import detect, generate_thumbnail
     [pytest.param(k, id=k.name) for k in (Path(__file__).parent.parent / "utils/tests").glob("test_ffmpeg/*")],
 )
 def test_detect(case: Path, snapshot_ffmpeg: SnapshotAssertion) -> None:
+    # NOTE:
+    # because we use docker to run ffmpeg, the input path need to be relative to the current working directory
+
     info = detect(str(case.relative_to(Path.cwd())))
     snapshot_ffmpeg(name=case.name) == info.dict()
 
     if info.type in ("video", "image"):
         temp_dir = tempfile.mkdtemp()  # Create a temporary directory
         thumbnail_path = os.path.join(temp_dir, f"{case.name}.thumbnail.jpg")  # Temporary thumbnail file path
-        generate_thumbnail(str(case), thumbnail_path)
+
+        generate_thumbnail(str(case.relative_to(Path.cwd())), thumbnail_path)
