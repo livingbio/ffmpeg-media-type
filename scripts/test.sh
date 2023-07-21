8515e699da18c4b94d64d29023275644d394922b
lucemia <lucemia@gmail.com>
2023-07-21 15:31:09 +0800

    fix test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # basic reference for writing script for travis
 
-curl -sSL https://install.python-poetry.org | python3 -
-
-poetry install --with test
-
-
 FFMPEG_DOCKER_VERSION=${1:-6.0} poetry run pytest ./src --cov=./src -s -m "ffmpeg_version" "$2" --cov-append
