6fef4440b59d1a04e59b13b4ed297171952e5a5c
Ian Hsueh <ian990436@gmail.com>
2023-10-17 12:07:46 +0800

    FfmpegMediaTypeError (#37)

---
+++ src/ffmpeg_media_type/exceptions.py
@@ -0,0 +1,2 @@
+class FfmpegMediaTypeError(Exception):
+    pass
--- src/ffmpeg_media_type/utils/shell.py
+++ src/ffmpeg_media_type/utils/shell.py
@@ -2,6 +2,8 @@ import os
 import shlex
 import tempfile
 
+from ..exceptions import FfmpegMediaTypeError
+
 
 def call(cmds: list[str]) -> str:
     # call command and return stdout
@@ -9,7 +11,8 @@ def call(cmds: list[str]) -> str:
     command = " ".join(shlex.quote(part) for part in cmds)
     with tempfile.TemporaryDirectory() as tmpdir:
         output_path = f"{tmpdir}/output.txt"
-        assert os.system(f"{command} > {output_path}") == 0, f"command failed: {command}"
+        if os.system(f"{command} > {output_path}") != 0:
+            raise FfmpegMediaTypeError(f"command failed: {command}")
 
         with open(output_path) as ifile:
             return ifile.read()
