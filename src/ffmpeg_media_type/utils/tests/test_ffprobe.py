e720b6219cdf95f49320d488724443f71e479bae
David Chen <lucemia@gmail.com>
2024-04-15 08:42:56 +0800

    refactor test case (#59)

--- src/ffmpeg_media_type/utils/tests/test_ffprobe.py
+++ src/ffmpeg_media_type/utils/tests/test_ffprobe.py
@@ -6,12 +6,10 @@ from syrupy.assertion import SnapshotAssertion
 from syrupy.extensions.json import JSONSnapshotExtension
 from syrupy.filters import paths
 
+from ...conftest import sample_test_media_files
 from ..ffprobe import ffprobe
 
 
-@pytest.mark.parametrize(
-    "case",
-    [pytest.param(k, id=k.name) for k in Path(__file__).parent.glob("data/*")],
-)
+@pytest.mark.parametrize("case", sample_test_media_files())
 def test_ffprobe_file(case: Path, snapshot: SnapshotAssertion) -> None:
     assert snapshot(extension_class=JSONSnapshotExtension, exclude=paths("format.filename")) == asdict(ffprobe(str(case.relative_to(Path.cwd()))))
